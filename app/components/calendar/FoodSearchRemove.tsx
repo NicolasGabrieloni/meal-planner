5d52e4d3a9ec33e885f0b88bbeed9b60988696e0
Alejo Paulon <alejo.paulon@gmail.com>
2023-10-04 17:09:42 -0300

    lil bit more done

--- app/components/calendar/FoodSearchRemove.tsx
+++ app/components/calendar/FoodSearchRemove.tsx
@@ -4,12 +4,14 @@ import RemoveButton from "./RemoveButton";
 
 export default function FoodSearchRemove() {
   return (
-    <div className="flex flex-row space-x-6 py-2">
+    <div className="flex flex-row justify-evenly py-4">
       <div>
         <LabelForInput labelText="Almuerzo" />
         <FoodInput presets={[]} />
       </div>
-      <RemoveButton />
+      <div className="flex place-items-end">
+        <RemoveButton />
+      </div>
     </div>
   );
 }

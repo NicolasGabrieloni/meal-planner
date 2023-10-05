f0e44a78f11e0976c339fa36f52eeb4ddea587ce
Alejo Paulon <alejo.paulon@gmail.com>
2023-10-05 09:30:11 -0300

    pull done, waiting for db data

--- app/(logged)/recipes/page.tsx
+++ app/(logged)/recipes/page.tsx
@@ -4,6 +4,14 @@ export const metadata: Metadata = {
   title: "Recetas",
 };
 
+interface Receta {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  ingredientes: string;
+  instrucciones: string;
+}
+
 async function Recetas() {
   const res = await fetch("http://localhost:3000/api/recipes");
   const data = await res.json();
@@ -15,12 +23,21 @@ async function Recipes() {
 
   return (
     <div>
-      {recetas.map((receta) => (
+      {recetas.map((receta: Receta) => (
         <div key={receta.id} className="ml-80 mt-10 w-96 text-center">
           <h2 className="m-5 font-bold">{receta.nombre}</h2>
-          <p><b>Descripcion: </b>{receta.descripcion}</p>
-          <p><b>ingredientes: </b>{receta.ingredientes}</p>
-          <p><b>instrucciones: </b>{receta.instrucciones}</p>
+          <p>
+            <b>Descripcion: </b>
+            {receta.descripcion}
+          </p>
+          <p>
+            <b>ingredientes: </b>
+            {receta.ingredientes}
+          </p>
+          <p>
+            <b>instrucciones: </b>
+            {receta.instrucciones}
+          </p>
         </div>
       ))}
     </div>

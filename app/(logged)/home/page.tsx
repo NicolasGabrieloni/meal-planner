22b6298eda436c8449167280f89ff64340766169
Alejo Paulon <alejo.paulon@gmail.com>
2023-10-28 18:37:50 -0300

    more responsiveness, added some more user info

--- app/(logged)/home/page.tsx
+++ app/(logged)/home/page.tsx
@@ -1,13 +1,10 @@
 import { Metadata } from "next";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import ListButton from "@/components/ui/ListButton";
 import { Buttton } from "@/components/ui/buttton";
 import Profile from "@/components/home/Profile";
+import { MiniCalendario } from "@/components/MiniCalendario";
+import { MiniDespensa } from "@/components/MiniDespensa";
+
 export const metadata: Metadata = {
   title: "Inicio",
 };
@@ -16,32 +13,16 @@ export default function Home() {
     <div className="flex flex-col items-center p-4">
       <Profile />
       <Buttton />
-      <div>
-        <div className=" mt-8 text-center">
-          <h2>Caledario semanal</h2>
-        </div>
-        <section className="mx-auto mt-5 w-80 rounded-lg border border-[#000000] bg-[#E9FFEB] p-5 shadow-xl">
-          <div>
-            <div className="flex justify-center">
-              <div className="h-[250px] "></div>
-            </div>
+      <div className="grid-cols-2 gap-4 md:grid ">
+        <div className="pt-8">
+          <MiniCalendario />
+          <div className="pt-8 md:items-center">
+            <ListButton label="Ver lista de compras" />
           </div>
-        </section>
-      </div>
-      <div className="mt-8">
-        <ListButton label="Ver lista de compras" />
-      </div>
-      <div>
-        <div className="mt-8 text-center">
-          <h2>Mi Despensa</h2>
         </div>
-        <section className="mx-auto mt-5 w-80 rounded-lg border border-[#000000] bg-[#E9FFEB] p-5 shadow-xl">
-          <div>
-            <div className="flex justify-center">
-              <div className="h-[350px] "></div>
-            </div>
-          </div>
-        </section>
+        <div className="pt-8">
+          <MiniDespensa />
+        </div>
       </div>
     </div>
   );
--- app/components/ui/carouselRecipes.tsx
+++ app/components/ui/carouselRecipes.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Carousel } from "@material-tailwind/react";
 import { Recetas } from "../ApiCalls";
 import { recipe } from "../Types";
+import Image from "next/image";
 
 interface LabelTypes {
   label: string;
@@ -30,7 +31,7 @@ export function CarouselRecipes({ label }: LabelTypes) {
         {label}
       </h1>
       <Carousel
-        className="h-[200px] w-[400px] rounded-xl"
+        className="h-[200px] w-[280px] rounded-xl"
         navigation={({ setActiveIndex, activeIndex, length }) => (
           <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
             {new Array(length).fill("").map((_, i) => (
@@ -48,9 +49,20 @@ export function CarouselRecipes({ label }: LabelTypes) {
         {randomRecipes.map((recetasFavs: recipe) => (
           <div
             key={recetasFavs.id}
-            className="relative z-30 mr-[-5px] h-[200px] w-[90px] transform-gpu rounded-md bg-slate-300 ring-4 ring-[#FAFAFA] transition-transform hover:z-40 hover:scale-110"
+            className="relative z-30 mr-[-5px] w-[300px] transform-gpu rounded-md bg-slate-300 ring-4 ring-[#FAFAFA] transition-transform hover:z-40 hover:scale-110"
           >
-            {recetasFavs.name}
+            <Image
+              src={recetasFavs.image}
+              alt={recetasFavs.name}
+              width={1200}
+              height={600}
+              className="h-[200px] w-[300px]"
+            />
+            <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/50">
+              <div className="w-3/4 text-center md:w-2/4">
+                <h1 className="text-2xl text-white"> {recetasFavs.name} </h1>
+              </div>
+            </div>
           </div>
         ))}
       </Carousel>

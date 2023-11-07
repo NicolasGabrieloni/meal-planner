b010dde7a67d6f066d1b16d1df325680252b46b7
Alejo Paulon <alejo.paulon@gmail.com>
2023-11-06 21:09:50 -0300

    adfa

--- app/api/updateFiles/route.ts
+++ app/api/updateFiles/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { randomUUID } from "crypto";
-const client = new S3Client();
+const client = new S3Client({ region: "us-east-1" });
 
 export const POST = async (req: NextRequest) => {
   try {

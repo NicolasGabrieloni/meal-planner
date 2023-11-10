054247acfe35280387c06a140cf3e2e3dab210ca
NicoDev <nicogabrieloni@gmail.com>
2023-11-10 14:40:23 -0300

    fixed

--- app/components/home/MiniCalendario.tsx
+++ app/components/home/MiniCalendario.tsx
@@ -41,7 +41,7 @@ export const MiniCalendario = () => {
         setMiniWeekMeals(updatedWeekMeals);
       });
     }
-  }, [userId]);
+  }, [userId, miniWeekMeals]);
 
   const daysOfWeek = [
     "Domingo",

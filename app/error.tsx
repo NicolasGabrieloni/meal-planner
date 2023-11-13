550f4bd7bbf4f8aa2c85633df8c342e8968e9fbb
Nicolás Gabrieloni <107143296+NicolasGabrieloni@users.noreply.github.com>
2023-11-13 18:11:58 -0300

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -11,8 +11,8 @@ export default function Error({
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+    console.error(error.cause);
+  }, [error.cause]);
 
   return (
     <div>
